ad823303e97ef7b5263576300faef741d86197b4
aNap73 <napolitano.anthony@yahoo.com>
2018-02-21 16:42:32 -0500

    villan object, run state villan select, fight loop init html/css changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
-SWGame ={ 
+SWGame ={
+  bInFight: false,
   gameMusicPlayer:document.getElementById("MusicToggle"), 
   gameSFXPlayer:document.getElementById("SFX"),
   gameMusic:$("#MusicToggle"),
@@ -6,16 +7,17 @@ SWGame ={
   bGameMusicOn: true,
   mToggle: function () {    
     if(SWGame.bGameMusicOn){
-      $("#MusicLabel").text("Music: Off");
+      $("#mnu2").attr("src","./assets/images/MusicOff.png");
       this.gameMusicPlayer.pause();
     }else{
       this.gameMusicPlayer.play();
-      $("#MusicLabel").text("Music: On");
+      $("#mnu2").attr("src","./assets/images/MusicOn.png");
     }
     SWGame.bGameMusicOn = (!SWGame.bGameMusicOn);
   },  
   gameState:"Splash",
   arrHerosChoosen: [],
+  arrVillansChoosen: [],
   reset: function (){
 
     SWGame.Hero.getRandomHero(SWGame.Hero);
@@ -37,14 +39,80 @@ SWGame ={
     $("#Hero1").attr("name", MyHero1Name);
     $("#Hero2").attr("name", MyHero2Name);
     $("#Hero3").attr("name", MyHero3Name);
+    
+  
+
+    SWGame.Villan.getRandomVillan(SWGame.Villan);
+    var MyVillan1Pic = SWGame.Villan.hPic;
+    var MyVillan1Name = SWGame.Villan.hName;
+  
+    SWGame.Villan.getRandomVillan(SWGame.Villan);
+    var MyVillan2Pic = SWGame.Villan.hPic;
+    var MyVillan2Name = SWGame.Villan.hName;
+
+    SWGame.Villan.getRandomVillan(SWGame.Villan);
+    var MyVillan3Pic = SWGame.Villan.hPic;
+    var MyVillan3Name = SWGame.Villan.hName;
+  
+    $("#Villan1").attr("src",MyVillan1Pic);
+    $("#Villan2").attr("src",MyVillan2Pic);
+    $("#Villan3").attr("src",MyVillan3Pic);
+  
+    $("#Villan1").attr("name", MyVillan1Name);
+    $("#Villan2").attr("name", MyVillan2Name);
+    $("#Villan3").attr("name", MyVillan3Name);
+    
+    
+
+    SWGame.arrHerosChoosen.length=0;
+    SWGame.arrVillansChoosen.length=0;
+    this.setGameState("Splash");    
+
+    $("#VillanStory1").text("");     
+    $(".vilport").width("98px");
+    $(".vilport").height("102px");
+    $("#BeginFightBut").hide();
+    
     $(".portrait").on('click',function () {
       SWGame.Hero.clickMyHero(SWGame.Hero,this);
       });
 
-    SWGame.arrHerosChoosen.length=0;
-    this.setGameState("Splash");    
+    $(".vilport").on('click',function () {
+      SWGame.Villan.clickMyVillan(SWGame.Villan,this);
+      });
+    $("#BeginFightBut").on('click', function (){
+      SWGame.Attack();        
+    });
+    $("#attkimg").attr("src","./assets/images/FightNow.png");
+    if(SWGame.bGameMusicOn){
+      SWGame.gameMusicPlayer.play();
+    } else{
+      SWGame.gameMusicPlayer.pause();
+    }
+    SWGame.bInFight = false;
   },
-  setGameState: function (inState){  
+  Attack: function(){
+    if(!SWGame.bInFight){
+      SWGame.gameSFX.attr("src","./assets/sounds/ISaber.mp3");
+      $("#attkimg").attr("src","./assets/images/Attack.png");
+      SWGame.bInFight = true;
+    }
+    else
+    {
+      //Do Attack
+      //Check for Win
+      //Check for Lose
+      var rnd = Math.random();
+      if(rnd > .5){
+        SWGame.gameSFX.attr("src","./assets/sounds/LSaber.mp3");
+      }else{
+        SWGame.gameSFX.attr("src","./assets/sounds/Blaster.mp3");
+      }
+    }
+    SWGame.gameSFXPlayer.play();
+  },
+  setGameState: function (inState){
+     
     this.gameState = inState;
     $(".main-content").hide();    
     switch(inState){
@@ -57,9 +125,12 @@ SWGame ={
        break;       
       case "Run":
        $("#Run").show();
-       this.gameMusic.attr("src","./assets/sounds/SWfight.mp3");
+       this.gameMusic.attr("src","./assets/sounds/SWFight.mp3");
        $("#MyHero").attr("src",SWGame.Hero.hPic);
        $("#HeroStory").text(SWGame.Hero.hText);
+
+       
+
        $(document.body).css("backgroundImage","url('./assets/images/DeathStar.jpg')");
        break;
       case "Over":
@@ -67,12 +138,12 @@ SWGame ={
        this.gameMusic.attr("src","./assets/sounds/Lose.mp3");
        break;
     }
-  },  
+  },    
   Hero:{
     arrhName:["Luke","Leia","Han","C3-PO","R2-D2","Chewie"],
     arrhPic: ["./assets/images/Luke.png","./assets/images/Leia.png","./assets/images/Han.png","./assets/images/C3.png","./assets/images/R2.png","./assets/images/Chewie.png"],
     arrhSoundBite: ["./assets/sounds/Luke.mp3","./assets/sounds/Leia.mp3","./assets/sounds/Han.mp3","./assets/sounds/C3PO.mp3","./assets/sounds/R2D2.mp3","./assets/sounds/Chewie.mp3"],
-    arrhFlavorText: ["After the celebrations on Yavin IV, Luke was sent on diplomatic missions to gain the help of outter rim warlords, gangsters and near do wells to find more willing soldiers and resources.  Little was he aware that he was being followed.","Leia was taken under the wing of the Rebel leader Mon Motha as a promising new commander. However, on a routine inspection mission their ship crashed landed on what they believed was an abandoned moon.  Unknown to them this would prove to be the fight of Leia’s life!" ,"Han was off to Tatooine. It was long overdue, he had to make payments to the Hutt's or he wouldn't be the pilot of the Falcon for much longer, heck he wouldn't be alive much longer.", "C3-PO incompetent lacky of R2 has gotten himself in a pickle not 3 minutes after R2 left on some foolish errand. Enrolling in a local rebellion coding bootcamp, he believes it's his last hope to quell the evaporator rebellion simmering on Tatooine.","R2-D2 leader of the Rebellion, has taken it upon himself to destroy the emperors newest evil battle station. R2 realizes that while everyone else stands around being useless, only he  can truly save the galaxy... Luke.. pfft...", "Chewie was seperated from Han after the battle on Yavin, it was time for him to return to his homeworld due to recent familiar events that portend of bad omens."],
+    arrhFlavorText: ["After the celebrations on Yavin IV, Luke was sent on a dangerous diplomatic missions to gain the help of outter rim warlords, gangsters and near do wells. Quickly he was betrayed however, and now is closely followed by imperial agents!","Leia was taken under the wing of the Rebel leader Mon Motha as a promising new commander. However, on a routine inspection mission their ship crashed landed on what they believed was an abandoned moon.  Unknown to them this would prove to be the fight of Leia’s life!" ,"Han was off to Tatooine. It was long overdue, he had to make payments to the Hutt's or he wouldn't be the pilot of the Falcon for much longer, heck he wouldn't be alive much longer. Unfortunately, the Falcon had other plans forcing Han to make repairs at a uncharted starport.", "C3-PO incompetent lacky of R2 has gotten himself in a pickle not 3 minutes after R2 left on some foolish errand. Enrolling in a local rebellion coding bootcamp, he believes it's his last hope to quell the evaporator rebellion simmering on Tatooine.  Unfortunately for our golden shiny friend, the Empire has other plans for him.","R2-D2 leader of the Rebellion, has taken it upon himself to destroy the emperors newest evil battle station. R2 realizes that while everyone else stands around being useless, only he  can truly save the galaxy... Luke.. pfft...", "Chewie was seperated from Han after the battle on Yavin, it was time for him to return to his homeworld due to recent events that portend of bad omens. Upon landing on his home planet, he was supprised to find his x girlfriend new boy friend was an impreial spy!"],
     arrhHP: ["200","75","150","60","70","180"],
     arrhDMG: ["20","10","15","7","15","18"],
     hName:"",
@@ -95,10 +166,10 @@ SWGame ={
     },
     clickMyHero: function(myHero, inElement){
       myHero = myHero.setMyHero(myHero,inElement.name);
-
-      SWGame.setGameState("Run");
       SWGame.gameSFX.attr("src",myHero.hSound);
-      SWGame.gameSFXPlayer.play();  
+      SWGame.gameSFXPlayer.play(); 
+      SWGame.setGameState("Run");
+ 
       return myHero;
     }, 
     setMyHero: function(myHero, myHerosName)
@@ -117,6 +188,86 @@ SWGame ={
       myHero.DMG = myHero.arrhDMG[i];
       myHero.hText = myHero.arrhFlavorText[i];
       return myHero;
+    }
+  },
+  Villan:{
+    arrhName:["Vader","Sidious","Maul","Sebulba","Boba","Dooku"],
+    arrhPic: ["./assets/images/Vader.png","./assets/images/Sidious.png","./assets/images/Maul.png","./assets/images/Sebulba.png","./assets/images/Boba.png","./assets/images/Duku.png"],
+    arrhSoundBite: ["./assets/sounds/Vader.mp3","./assets/sounds/Sidious.mp3","./assets/sounds/Maul.mp3","./assets/sounds/Sebulba.mp3","./assets/sounds/BobaFett.mp3","./assets/sounds/Dooku.mp3"],
+    arrhFlavorText: ["This brooding lord of the sith, teeming with anger and hatred, reaches out with the force and detects your presense. Now nothing can save you from the wrath of Darth Vader.","You truly have stumbled upon a plot of great consequence, for at it's center is the Emperor himself, Lord Sidious!","You are confronted with one of the most dangerous and unique weapons in the known universe a dual bladed lightsaber.  Which can only mean one thing, Darth Maul has found you.","This notorious gangster, formerly a champion pod racer, has fallen on hard times. However his mean streak is still wider than the back end of a Hutt. Best steer clear of this one.", "No one has ever escaped Boba Fett before. You think you can best the best of the best... Boba thinks your just going to die tired.","This Dark Lord could only be defeated by a young Vader, having even escaped master Yoda and Obi-Wan at one point, ... enough said.... prepare to meet Count Dooku!"],
+    arrhHP: ["200","75","150","60","70","180"],
+    arrhDMG: ["20","10","15","7","15","18"],
+    hName:"",
+    hPic:"",    
+    hSound:"",
+    hText:"",
+    hHP: 0,
+    hDMG: 0,    
+    getRandomVillan: function(myVillan)
+    {
+      var myrnd = Math.floor((Math.random()*6));
+      while(SWGame.arrVillansChoosen.includes(myrnd)){
+        myrnd = Math.floor((Math.random()*6)); 
+      } 
+      SWGame.arrVillansChoosen.push(myrnd);
+
+      
+      myVillan = myVillan.setVillan(myVillan, myrnd);
+      return myVillan;
+    },
+    clickMyVillan: function(myVillan, inElement){
+      if(SWGame.bInFight){return myVillan};
+      myVillan = myVillan.setMyVillan(myVillan,inElement.name);
+
+      $("#MyVillan").attr("src",myVillan.hPic);
+            
+      $("#VillanStory1").text("");     
+      $(".vilport").width("98px");
+      $(".vilport").height("102px");
+      
+      
+      $("#VillanStory1").text(myVillan.hText);
+      $("#BeginFightBut").show();
+      switch(inElement.id)
+      {
+        case "Villan1":
+          
+          $("#Villan1").width(98 * 1.8);
+          $("#Villan1").height(102 * 1.8);
+          break;
+        case "Villan2":
+          //$("#VillanStory2").text(myVillan.hText);
+          $("#Villan2").width(98 * 1.8);
+          $("#Villan2").height(102 * 1.8);
+          break;
+        case "Villan3":
+          //$("#VillanStory3").text(myVillan.hText);
+          $("#Villan3").width(98 * 1.8);
+          $("#Villan3").height(102 * 1.8);
+          break;
+      }
+      
+    
+      SWGame.gameSFX.attr("src",myVillan.hSound);
+      SWGame.gameSFXPlayer.play();  
+      return myVillan;
+    }, 
+    setMyVillan: function(myVillan, myVillansName)
+    {
+      
+      myVillan = myVillan.setVillan(myVillan, myVillan.arrhName.indexOf(myVillansName));
+    
+      return myVillan;
+    },
+    setVillan: function(myVillan, i)
+    {
+      myVillan.hName = myVillan.arrhName[i];
+      myVillan.hPic = myVillan.arrhPic[i];
+      myVillan.hHP = myVillan.arrhHP[i];
+      myVillan.hSound = myVillan.arrhSoundBite[i];
+      myVillan.DMG = myVillan.arrhDMG[i];
+      myVillan.hText = myVillan.arrhFlavorText[i];
+      return myVillan;
     }
   }
   
